b912c10f291a5f1e0952b5540eb53d1056c40fbb
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-03-05 23:10:20 -0500

    fix: solved issue where frontend was not finding aliased paths

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,25 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import { useState } from 'react'
-import ProtectedRoute from '@/components/protectedRoute';
-import Login from '@/views/login';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router';
+// import { useState } from 'react'
+// import ProtectedRoute from '@/components/protectedRoute';
+// import Login from '@/views/login';
 import Dashboard from '@/views/dashboard';
-import Questions from '@/views/questions';
+// import Questions from '@/views/questions';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  // const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   return (
-   <Router>
-      <Routes>
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+    <div className="flex flex-col w-full h-screen">
+      <h1>Nummus Budget</h1>
+      <p>Welcome to the Nummus Budget app! This is a simple budgeting tool built with a Node.js backend and a React frontend. Use the navigation links to explore the dashboard and manage your budget.</p>
+      <Dashboard />
+    </div>
+  //  <Router>
+  //     <Routes>
+  //       <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
 
-        {/* Protected Routes Group */}
-        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/questions" element={<Questions />} />
-        </Route>
-      </Routes>
-    </Router>
+  //       <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+  //         <Route path="/dashboard" element={<Dashboard />} />
+  //         <Route path="/questions" element={<Questions />} />
+  //       </Route>
+  //     </Routes>
+  //   </Router>
   )
 }
 
