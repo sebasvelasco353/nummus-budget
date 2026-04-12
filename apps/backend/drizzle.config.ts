2015c9f68a027eb5d68e09863f48817653ec8c9d
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-04-11 20:13:14 -0500

    feat(database): created migration files using drizzle cli migrate command

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import { getDatabaseUrl } from './utils/config';
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: getDatabaseUrl(),
   },
 });
 
